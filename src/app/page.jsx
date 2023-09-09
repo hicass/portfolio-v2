239c2cae5b11017b8b2e32620bbc3ddc5d062864
hicass <cassidyjwalters@gmail.com>
2023-09-09 01:30:04 -0700

    Set up main page as well as fonts.

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import './styles/main.css'
+
+
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <h2>hi i'm</h2>
+        <h1>Cass</h1>
+        <p id='mid-bio'>
+          I am a Software Developer who weaves<br /> 
+          code and dreams into tangible realities.
+        </p>
+      </section>
+    </main>
+  )
+}
