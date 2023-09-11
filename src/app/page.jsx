9a22d238a541b050782b0105d9131d95a10e7c21
hicass <cassidyjwalters@gmail.com>
2023-09-11 12:16:55 -0700

    Move the About Section into its own component for better modularity.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,7 @@ import boops from '../../public/images/boops.png'
 import tendrils1 from '../../public/images/wave-tendrils.png'
 import tendrils2 from '../../public/images/tendrils.png'
 import GlowingCursor from './components/GlowingCursor'
+import AboutSection from './components/AboutSection'
 import { FaReact, FaGitAlt, FaCss3Alt } from 'react-icons/fa';
 import { BiLogoJavascript, BiLogoNodejs, BiLogoMongodb, BiLogoPython, BiLogoDjango } from 'react-icons/Bi';
 import { TbBrandNextjs } from 'react-icons/Tb';
@@ -66,31 +67,7 @@ export default function Home() {
           </div>
         </div>
 
-        <section id='about' className='content-area'>
-          <Fade>
-            <h3>about me!</h3>
-            <div id='about-contents'>
-              <p className='about-me-p'>
-                  Creativity is at the heart of everything I do. I view coding as an art 
-                  form, where every line of code has the potential to be elegant and 
-                  expressive. I value the importance of visual appeal, attention to 
-                  detail, and believe in the power that blending creativity and technology has.
-              </p>
-              <p className='about-me-p'>
-                  As a coding bootcamp graduate, I have developed a strong aptitude 
-                  for fast learning, enabling me to quickly grasp new concepts and 
-                  technologies. I thrive on the excitement of exploring and mastering 
-                  cutting-edge tools and frameworks, continuously expanding my skillset.
-              </p>
-              <p className='about-me-p'>
-                  Beyond the code, I am an artist who loves expressing myself 
-                  through digital mediums. I also enjoy getting out into nature,
-                  spending quality time with my dog and partner, and diving into the immersive 
-                  worlds of video games when it’s time to unwind.
-              </p>
-              </div>
-            </Fade>
-        </section>
+        <AboutSection />
 
         <section id='skills' className='content-area'>
           <Slide direction='left' triggerOnce='true'>
@@ -131,8 +108,8 @@ export default function Home() {
           </div>
         </section>
 
-        <GlowingCursor />
       </div>
+        <GlowingCursor />
     </main>
   )
 }
