462c00638626beab615748891ea51130168e9905
hicass <cassidyjwalters@gmail.com>
2023-09-12 19:37:54 -0700

    Add confimation message when contact form is submitted.

--- src/app/components/ContactSection.jsx
+++ src/app/components/ContactSection.jsx
@@ -1,21 +1,37 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
 import { AiOutlineMail } from 'react-icons/Ai'
+import { BsSendCheck } from 'react-icons/Bs'
 
 
 export default function ContactSection() {
-  const form = useRef();
+  const [formSubmitted, setFormSubmitted] = useState(false)
+  const [formValues, setFormValues] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+  const form = useRef()
 
   const sendEmail = (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     emailjs.sendForm('contact_service', 'contact_form', form.current, '-qDI4dvV-5_y28-od')
       .then((result) => {
-          console.log(result.text);
+          console.log(result.text)
       }, (error) => {
-          console.log(error.text);
-      });
-  };
+          console.log(error.text)
+      })
+  }
+
+  const handleSubmit = () => {
+    setFormSubmitted(true)
+    setFormValues({
+      name: '',
+      email: '',
+      message: ''
+    })
+  }
 
   return (
     <section id='contact' className='content-area'>
@@ -23,16 +39,53 @@ export default function ContactSection() {
 
       <div id='contact-contents'>
         <form className='contact-form' ref={form} onSubmit={sendEmail}>
+
+            {formSubmitted && 
+              <div className='submit-confirmation'>
+                <BsSendCheck className='submit-icon' />
+                <p>
+                  Thank you for reaching out, talk to you soon!
+                </p>
+              </div>
+            }
+
             <label className='form-label'>Name</label>
-            <input className='form-input' type='text' name='from_name' />
+            <input 
+              className='form-input' 
+              type='text' 
+              name='from_name' 
+              value={formValues.name}
+              onChange={(e) =>
+                setFormValues({ ...formValues, name: e.target.value })
+              }
+            />
 
             <label className='form-label'>Email</label>
-            <input className='form-input' type='email' name='from_email' />
+            <input 
+              className='form-input' 
+              type='email' 
+              name='from_email' 
+              value={formValues.email}
+              onChange={(e) =>
+                setFormValues({ ...formValues, email: e.target.value })
+              }
+            />
 
             <label className='form-label'>Message</label>
-            <textarea name='message'/>
+            <textarea 
+              name='message'
+              value={formValues.message}
+              onChange={(e) =>
+                setFormValues({ ...formValues, message: e.target.value })
+              }
+            />
 
-            <input className='form-submit' type='submit' value='Send' />
+            <input 
+              className='form-submit' 
+              type='submit' 
+              value='Send' 
+              onClick={handleSubmit}
+            />
           </form>
 
         <p>
