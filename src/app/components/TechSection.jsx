a4ee65975cfeea86f4dc399c8a59db4a88b14f89
hicass <cassidyjwalters@gmail.com>
2023-09-28 12:44:45 -0700

    Fix spacing for technologies in the tech section and projects, and deploy to Heroku.

--- src/app/components/TechSection.jsx
+++ src/app/components/TechSection.jsx
@@ -19,37 +19,37 @@ export default function TechSection() {
       </Slide>
 
       <Slide direction="right" triggerOnce="true" fraction="1">
-        <div className="skill-icon-container">
-          <div className="react-icon icon-container">
-            <FaReact />
-            <p className="icon-name">React</p>
-          </div>
+          <div className="skill-icon-container">
+            <div className="react-icon icon-container">
+              <FaReact />
+              <p className="icon-name">React</p>
+            </div>
 
-          <div className="next-icon icon-container">
-            <TbBrandNextjs className="skill-icon" />
-            <p className="icon-name">Next.js</p>
-          </div>
+            <div className="next-icon icon-container">
+              <TbBrandNextjs className="skill-icon" />
+              <p className="icon-name">Next.js</p>
+            </div>
 
-          <div className="git-icon icon-container">
-            <FaGitAlt className="skill-icon" />
-            <p className="icon-name">Git</p>
-          </div>
+            <div className="git-icon icon-container">
+              <FaGitAlt className="skill-icon" />
+              <p className="icon-name">Git</p>
+            </div>
 
-          <div className="node-icon icon-container">
-            <BiLogoNodejs className="skill-icon" />
-            <p className="icon-name">Node.js</p>
-          </div>
+            <div className="node-icon icon-container">
+              <BiLogoNodejs className="skill-icon" />
+              <p className="icon-name">Node.js</p>
+            </div>
 
-          <div className="mongo-icon icon-container">
-            <BiLogoMongodb className="skill-icon" />
-            <p className="icon-name">MongoDB</p>
-          </div>
+              <div className="mongo-icon icon-container">
+                <BiLogoMongodb className="skill-icon" />
+                <p className="icon-name">MongoDB</p>
+              </div>
 
-          <div className="express-icon icon-container">
-            <SiExpress className="skill-icon" />
-            <p className="icon-name">Express.js</p>
+              <div className="icon-container">
+                <SiExpress className="skill-icon" />
+                <p className="icon-name">Express.js</p>
+              </div>
           </div>
-        </div>
       </Slide>
     </section>
   );
